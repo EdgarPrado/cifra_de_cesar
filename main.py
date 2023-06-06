2c047c324fde425156f8427ad836d5d335c5d02d
Edgar Prado <joaoedgarpr2@gmail.com>
2023-06-06 08:28:49 -0300

    Foi trocado o () da linha 14 e colocado para executar no Lower da linha 2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 direcao = input("Digite 'encode' para codificar e 'decode' para descodificar.\n")
-texto = input("Digite a sua mensagem.\n").lower
+texto = input("Digite a sua mensagem.\n").lower()
 espaco = int(input("Quantas casas de locomocao terá?\n"))
+
+
 def cesar():
     texto_fin = ""
     if direcao == "encode":
-        for letter in texto():
+        for letter in texto:
             posicao = ord(f"{letter}")
             nova_posicao = posicao + espaco
             nova_letra = chr(nova_posicao)
@@ -17,4 +19,6 @@ def cesar():
             nova_letra = chr(nova_posicao)
             texto_fin += nova_letra
         print(f"The decoded text is {texto_fin}")
+
+
 cesar()
